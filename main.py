cea8dd2e122d34b074258e77572d7a019ba608ba
kundan200 <80200357+kundan200@users.noreply.github.com>
2024-05-07 04:32:10 +0530

    main.py

--- main.py
+++ main.py
@@ -51,4 +51,5 @@ def speech_to_text():
         return render_template('index.html', query=query, response=response)
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
